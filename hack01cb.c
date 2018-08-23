e02001e7cf3513cdb448a3250b786e83524b370c
Carlie Belt <carlie.r.belt@gmail.com>
2018-08-23 14:10:05 -0500

    changed hello world to name and major

---
+++ hack01cb.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Carlie Belt
+ * Date: 2018/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Carlie, Computer Engineering Major\n");
+
+  return 0;
+}
